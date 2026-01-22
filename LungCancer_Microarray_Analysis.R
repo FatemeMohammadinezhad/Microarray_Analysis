5004a51206e0b6f36e520bdb64b428fc406d533d
Fateme Mohammadinezhad <mohammadinezhadfateme@gmail.com>
2026-01-22 20:40:36 +0100

    Updated analysis

--- LungCancer_Microarray_Analysis.R
+++ LungCancer_Microarray_Analysis.R
@@ -39,5 +39,47 @@ arrayQualityMetrics(expressionset = raw_data,
                     outdir = "Results_lungcancer/QC_raw_data",
                     force = TRUE,
                     do.logtransform = TRUE)
+normalized_data<-rma(raw_data)
+arrayQualityMetrics(expressionset = normalized_data[,c(16,17,35,36,39,41,45,46,
+                                                       65,66,95,96,
+                                                       99,102,106)],
+                    outdir = "Results_lungcancer/QC_normalized_data",
+                    force = TRUE,
+                    reporttitle = "QC normalized subset (15 outlier samples)")
 
+# In the normalized results, we can see that the problem of outlier samples now are solved # 
 
+class(normalized_data)
+processed_data<-as.data.frame(exprs(normalized_data))
+dim(processed_data)
+#-------------------------------------------------------------------------------
+
+### Filter low-values probes ==> not strongly contributing in biological info
+row_median<-rowMedians(as.matrix(processed_data))
+row_median
+hist(row_median,
+     breaks = 100,
+     freq = FALSE,
+     main = "Median Intensity Distribution",
+     col = "blue",
+     border = "white")
+dev.off()
+dev.list()
+threshold=4.5
+abline(v=threshold,col="red",lwd=2.5)
+index<-row_median>threshold
+filtered_data<-processed_data[index,]
+
+### Change col names based on the phenotype data raw names
+colnames(filtered_data)<-rownames(phenotype_data)
+processed_data<-filtered_data
+#-------------------------------------------------------------------------------
+### Classifying target col names in phenotype data
+
+class(phenotype_data$source_name_ch1)
+groups<-factor(phenotype_data$source_name_ch1,
+               levels = c("frozen tissue of primary tumor",
+                          "frozen tissue of adjacent normal"),
+               labels = c("cancer","normal"))
+class(groups)
+levels(groups)
